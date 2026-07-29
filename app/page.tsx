c0c522aa37d10eee7da0d4a1c134b1b380c70241
kazirobinking <kazirobinking@gmail.com>
2026-07-29 21:43:42 +0600

    hero update

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import ClassRoutineSection from "@/components/ClassRoutineSection";
 import FaqSection from "@/components/FaqSection";
 import Hero from "@/components/Hero";
 import { Roadmap } from "@/components/Roadmap";
+import Voice from "@/components/Voice";
 import Image from "next/image";
 
 export default function Home() {
   return (
   <>
   <Hero/>
+  <Voice/>
   <Roadmap/>
   <ClassRoutineSection/>
   <FaqSection/>
